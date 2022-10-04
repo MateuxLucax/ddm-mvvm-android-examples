3638b3c697d615ba0cbc045abb5e5288e434800e
Mateus Lucas <mateuxlucax@gmail.com>
2022-10-03 21:01:11 -0300

    test: add initial test for SearchCepViewModel

---
+++ c-cep/app/src/test/java/com/example/cep/presentation/search/SearchCepViewModelTest.java
@@ -0,0 +1,14 @@
+package com.example.cep.presentation.search;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+// TODO: make this test
+
+public class SearchCepViewModelTest {
+
+    @Test
+    public void searchCep() {
+    }
+}
